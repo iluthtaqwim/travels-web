b06ab65d25de847aae661c1e109f3caff1916afd
azizzabdul <45441929+azizzabdul@users.noreply.github.com>
2022-07-05 11:01:22 +0700

    add->CRUD gallery

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::prefix('admin')
             ->name('dashboard');
 
         Route::resource('travel-package', 'TravelPackageController');
+        Route::resource('gallery', 'GalleryController');
     });
 
 
